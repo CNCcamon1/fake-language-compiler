295b5968f8608b4b1038f4ae32b85e87b7d9dfbc
Camon Crocker <crockecn@mail.uc.edu>
2023-01-26 12:33:41 -0500

    Finishing up parser refactor

--- parser.cpp
+++ parser.cpp
@@ -23,37 +23,43 @@ Token* get_token_at_index(std::vector<Token*>* encounteredTokens, int currentTok
 int parse_nonterminal(ParseTreeNode* node, ScannerParams* scannerParams, 
     std::vector<Token*>* encounteredTokens, int currentTokenIndex,
     Grammar* grammar){
-        std::vector<GrammarOption*>* childOptions = grammar->get_possible_children_for_type(node->type);
-        int childOptionIndex = 0;
-        bool allChildrenParsed = false;
-        while(allChildrenParsed == false){
-            GrammarOption* childOption = childOptions->at(childOptionIndex);
-            if(childOption == nullptr){
-                allChildrenParsed = true;
-            }
-            else{
-                Token* currentToken = get_token_at_index(encounteredTokens, currentTokenIndex, scannerParams);
-                Token* lookaheadToken = get_token_at_index(encounteredTokens, currentTokenIndex+1, scannerParams);
-                if(grammar->child_choice_valid(childOption->tokenType, currentToken, lookaheadToken)){
-                    ParseTreeNode* newChild = new ParseTreeNode(childOption->tokenType);
-                    currentTokenIndex = parse_node(newChild, scannerParams, encounteredTokens, currentTokenIndex, grammar);
-                    node->add_child(newChild);
-                    if(childOption->optionType != ARRAY_C){
-                        childOptionIndex++;
+        std::vector<std::vector<GrammarOption*>>* possibleChildrenArrangements = grammar->get_possible_children_for_type(node->type);
+        for(int i=0; i<possibleChildrenArrangements->size(); i++){
+            //For each derivation option for the nonterminal
+            Token* currentToken = get_token_at_index(encounteredTokens, currentTokenIndex, scannerParams);
+            //First check if the current token is a valid first choice for this derivation option
+            if(grammar->child_choice_valid(possibleChildrenArrangements->at(i).at(0)->tokenType, currentToken)){
+                //If it is, use this derivation option and parse it
+                std::vector<GrammarOption*>* chosenDerivationOption = &possibleChildrenArrangements->at(i);
+                int grammarOptionIndex = 0;
+                while(grammarOptionIndex < chosenDerivationOption->size()){
+                    //For each token in the chosen derivation option, parse it and add it as a child to the current node
+                    GrammarOption* currentGrammarOption = chosenDerivationOption->at(grammarOptionIndex);
+                    currentToken = get_token_at_index(encounteredTokens, currentTokenIndex, scannerParams);
+                    if(grammar->child_choice_valid(currentGrammarOption->tokenType, currentToken)){
+                        //If the current token is a valid first choice for the current grammar option, parse it
+                        ParseTreeNode* newChild = new ParseTreeNode(currentGrammarOption->tokenType);
+                        currentTokenIndex = parse_node(newChild, scannerParams, encounteredTokens, currentTokenIndex, grammar);
+                        node->add_child(newChild);
+                        if(currentGrammarOption->optionType != ARRAY_C){
+                            grammarOptionIndex++;
+                        }
                     }
-                }
-                else{
-                    if(childOption->optionType == ARRAY_C || childOption->optionType == OPTIONAL_C){
-                        childOptionIndex++;
+                    else if(currentGrammarOption->optionType == ARRAY_C || currentGrammarOption->optionType == OPTIONAL_C){
+                        //If a mismatch occurs with an array or optional grammar option, then just advance to the next grammar option
+                        grammarOptionIndex++;
                     }
                     else{
+                        //
                         throw NoValidDerivationsException();
                     }
                 }
-            }
 
+                return currentTokenIndex;
+            }
         }
-        return currentTokenIndex;
+
+        throw NoValidDerivationsException();
 }
 
 int parse_identifier(ParseTreeNode* node, ScannerParams* scannerParams, 
@@ -89,6 +95,7 @@ ParseTree* parse_file(struct ScannerParams* scannerParams){
         ParseTree* parsedTree = new ParseTree(new ParseTreeNode(PROGRAM_NT));
         std::vector<Token*> encounteredTokens;
         Grammar* grammar = new Grammar();
+        grammar->build_grammar();
         encounteredTokens.push_back(scan(scannerParams));
         encounteredTokens.push_back(scan(scannerParams));
         parse_node(parsedTree->rootNode, scannerParams, &encounteredTokens, 0, grammar);
--- types/grammar.h
+++ types/grammar.h
@@ -2,29 +2,36 @@
 
 class Grammar{
     public:
-        std::vector<GrammarOption*>* get_possible_children_for_type(TokenType type){
+        std::vector<std::vector<GrammarOption*>>* get_possible_children_for_type(TokenType type){
             return &grammarOptionsTable[type-43];
         }
 
-        bool child_choice_valid(TokenType type, Token* currentToken, Token* lookaheadToken){
-            bool firstValid = false;
-            //bool followsValid = false;
-
-            std::vector<TokenType>* firstPossibilities = &firstTokenTable[type];
-            //std::vector<TokenType>* followsPossibilities = &followTokenTable[type];
-
-            for(int i=0; i<firstPossibilities->size(); i++){
-                if(firstPossibilities->at(i) == currentToken->type){
-                    firstValid = true;
+        //Returns whether or not the current token is a valid first token for the given type
+        bool child_choice_valid(TokenType type, Token* currentToken){
+            if(is_terminal(type)){
+                return type == currentToken->type;
+            }
+            else{
+                bool firstValid = false;
+                std::vector<TokenType>* firstPossibilities = &firstTokenTable[type];
+                for(int i=0; i<firstPossibilities->size(); i++){
+                    if(firstPossibilities->at(i) == currentToken->type){
+                        firstValid = true;
+                    }
                 }
+
+                return firstValid;
             }
 
-            return firstValid;
         }
 
-        bool is_terminal(TokenType currentType);
+        bool is_terminal(TokenType currentType){
+            return currentType < 43;
+        }
 
-        void build_grammar();
+        void build_grammar(){
+            build_first_token_table();
+        }
     
     private:
         std::vector<std::vector<std::vector<GrammarOption*>>> grammarOptionsTable {
@@ -37,7 +44,7 @@ class Grammar{
             {{new GrammarOption(PROCEDURE_HEADER_NT), new GrammarOption(PROCEDURE_BODY_NT)}}, //PROCEDURE_DECLARATION_NT
             {{new GrammarOption(VARIABLE_T), new GrammarOption(IDENTIFIER_NT), new GrammarOption(COLON_T), new GrammarOption(TYPE_MARK_NT), new GrammarOption(BOUND_OBJ_NT, OPTIONAL_C)}}, //VARIABLE_DECLARATION_NT
             {{new GrammarOption(PROCEDURE_T), new GrammarOption(IDENTIFIER_NT), new GrammarOption(COLON_T), new GrammarOption(TYPE_MARK_NT), new GrammarOption(OPEN_PARENTHESIS_T), new GrammarOption(PARAMETER_LIST_NT, OPTIONAL_C), new GrammarOption(CLOSE_PARENTHESIS_T)}}, //PROCEDURE_HEADER_NT
-            {{new GrammarOption(DECLARATION_OBJ_NT, ARRAY__C), new GrammarOption(BEGIN_T), new GrammarOption(STATEMENT_OBJ_NT, ARRAY_C), new GrammarOption(END_T), new GrammarOption(PROCEDURE_T)}}, //PROCEDURE_BODY_NT
+            {{new GrammarOption(DECLARATION_OBJ_NT, ARRAY_C), new GrammarOption(BEGIN_T), new GrammarOption(STATEMENT_OBJ_NT, ARRAY_C), new GrammarOption(END_T), new GrammarOption(PROCEDURE_T)}}, //PROCEDURE_BODY_NT
             {{new GrammarOption(INTEGER_T)}, {new GrammarOption(FLOAT_T)}, {new GrammarOption(STRING_T)}, {new GrammarOption(BOOL_T)}}, //TYPE_MARK_NT
             {{new GrammarOption(PARAMETER_NT), new GrammarOption(PARAMETER_LIST_OBJ_NT, OPTIONAL_C)}}, //PARAMETER_LIST_NT
             {{new GrammarOption(VARIABLE_DECLARATION_NT)}}, //PARAMETER_NT
@@ -50,7 +57,7 @@ class Grammar{
             {{new GrammarOption(IDENTIFIER_NT), new GrammarOption(OPEN_PARENTHESIS_T), new GrammarOption(ARGUMENT_LIST_NT, OPTIONAL_C)}}, //PROCEDURE_CALL_NT
             {{new GrammarOption(EXPRESSION_NT), new GrammarOption(ARGUMENT_LIST_OBJ_NT)}}, //ARGUMENT_LIST_NT
             {{new GrammarOption(IDENTIFIER_NT), new GrammarOption(EXPRESSION_BLOCK_OBJ_NT, OPTIONAL_C)}}, //DESTINATION_NT
-            {{new GrammarOption(NOT_T, OPTIONAL_C), new GrammarOption(ARITH_OP_NT), new GrammarOption(EXPRESSION_RECURSE_NT, OPTIONAL_C)}}, //EXPRESSION_NT
+            {{new GrammarOption(NOT_OP_T, OPTIONAL_C), new GrammarOption(ARITH_OP_NT), new GrammarOption(EXPRESSION_RECURSE_NT, OPTIONAL_C)}}, //EXPRESSION_NT
             {{new GrammarOption(RELATION_NT), new GrammarOption(ARITH_OP_RECURSE_NT, OPTIONAL_C)}}, //ARITH_OP_NT
             {{new GrammarOption(TERM_NT), new GrammarOption(RELATION_RECURSE_NT, OPTIONAL_C)}}, //RELATION_NT
             {{new GrammarOption(FACTOR_NT), new GrammarOption(TERM_RECURSE_NT, OPTIONAL_C)}}, //TERM_NT
@@ -59,7 +66,7 @@ class Grammar{
             {{}}, //STRING_NT
             {{new GrammarOption(DECLARATION_NT), new GrammarOption(SEMICOLON_T)}}, //DECLARATION_OBJ_NT
             {{new GrammarOption(STATEMENT_NT), new GrammarOption(SEMICOLON_T)}}, //STATEMENT_OBJ_NT
-            {{new GrammarOption(AND_T), new GrammarOption(ARITH_OP_NT)}, {new GrammarOption(OR_T), new GrammarOption(ARITH_OP_NT)}}, //EXPRESSION_RECURSE_NT
+            {{new GrammarOption(AND_OP_T), new GrammarOption(ARITH_OP_NT)}, {new GrammarOption(OR_OP_T), new GrammarOption(ARITH_OP_NT)}}, //EXPRESSION_RECURSE_NT
             {{}}, //STRING_LITERAL_NT
             {{new GrammarOption(PLUS_OP_T), new GrammarOption(RELATION_NT)}, {new GrammarOption(MINUS_OP_T), new GrammarOption(RELATION_NT)}}, //ARITH_OP_RECURSE_NT
             {{new GrammarOption(LESSTHAN_OP_T), new GrammarOption(TERM_NT)}, {new GrammarOption(GREATER_EQUAL_OP_T), new GrammarOption(TERM_NT)}, {new GrammarOption(LESS_EQUAL_OP_T), new GrammarOption(TERM_NT)}, {new GrammarOption(GREATERTHAN_OP_T), new GrammarOption(TERM_NT)}, {new GrammarOption(EQUALITY_OP_T), new GrammarOption(TERM_NT)}, {new GrammarOption(INEQUALITY_OP_T), new GrammarOption(TERM_NT)}}, //RELATION_RECURSE_NT
@@ -67,7 +74,7 @@ class Grammar{
             {{new GrammarOption(OPEN_BRACKET_T), new GrammarOption(BOUND_NT), new GrammarOption(CLOSE_BRACKET_T)}}, //BOUND_OBJ_NT
             {{new GrammarOption(COMMA_T), new GrammarOption(PARAMETER_LIST_NT)}}, //PARAMETER_LIST_OBJ_NT
             {{new GrammarOption(COMMA_T), new GrammarOption(ARGUMENT_LIST_NT)}}, //ARGUMENT_LIST_OBJ_NT
-            {{new GrammarOption(ELSE_T), new GrammarOption(STATEMENT_BLOCK_NT, ARRAY_C)}}, //ELSE_BLOCK_NT
+            {{new GrammarOption(ELSE_T), new GrammarOption(STATEMENT_OBJ_NT, ARRAY_C)}}, //ELSE_BLOCK_NT
             {{new GrammarOption(OPEN_BRACKET_T), new GrammarOption(EXPRESSION_NT), new GrammarOption(CLOSE_BRACKET_T)}}, //EXPRESSION_BLOCK_OBJ_NT
         };
 
@@ -75,7 +82,29 @@ class Grammar{
 
         };
 
-        /*std::vector<std::vector<TokenType>> followTokenTable{
+        std::vector<TokenType> get_valid_first_tokens(TokenType nonterminalType){
+            std::vector<TokenType> validFirstTokens;
+            std::vector<std::vector<GrammarOption*>>* derivationOptionsForNonterminal = &grammarOptionsTable[nonterminalType];
+            for(int i=0; i<derivationOptionsForNonterminal->size(); i++){
+                //For each derivation option for this nonterminal
+                GrammarOption* first = derivationOptionsForNonterminal->at(i).at(0);
+                if(is_terminal(first->tokenType) || first->tokenType == IDENTIFIER_NT || first->tokenType == NUMBER_NT || first->tokenType == STRING_LITERAL_NT){
+                    validFirstTokens.push_back(first->tokenType);
+                }
+                else{
+                    std::vector<TokenType> typeOptionsForNonterminal = get_valid_first_tokens(first->tokenType);
+                    validFirstTokens.insert(validFirstTokens.end(), typeOptionsForNonterminal.begin(), typeOptionsForNonterminal.end());
+                }
+            }
 
-        }; */
+            return validFirstTokens;
+        }
+
+        void build_first_token_table(){
+            for(int i=0; i<grammarOptionsTable.size(); i++){
+                //For each nonterminal type in the grammar
+                std::vector<TokenType> firstTokens = get_valid_first_tokens(static_cast<TokenType>(i));
+                firstTokenTable.push_back(firstTokens);
+            }
+        }
 };
